f01b20134e7261a7f3e65fa5a6b949734d5abbfb
Nikhil Choudhary <116430126+Nikhil2072001@users.noreply.github.com>
2023-11-14 17:08:00 +0530

    last2

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,21 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-const Login = (isLoggedIn, setIsLoggedIn) => {
-  const [data, setData] = useState({ username: "", password: "" });
-  // function handleChange(e){
-  //   setData({...data, [e.target.name]:e.target.value});
-  // }
+const Login = ({ isLoggedIn, setIsLoggedIn }) => {
+  const [data, setData] = useState({ username: '', password: '' });
+
   const handleChange = (e) => {
     console.log(e.target, e.target.name);
     setData({ ...data, [e.target.name]: e.target.value });
   };
+
   return (
     <div>
       {isLoggedIn ? (
-        <p>you are logged in!</p>
+        <p>You are logged in!</p>
       ) : (
         <form>
           <div>
-            <label htmlFor="username">Username</label>
+            <label htmlFor="username">Username:</label>
             <input
               type="text"
               id="username"
@@ -26,12 +25,12 @@ const Login = (isLoggedIn, setIsLoggedIn) => {
             />
           </div>
           <div>
-            <label htmlFor="password">Password</label>
+            <label htmlFor="password">Password:</label>
             <input
               type="password"
-id="password"
               name="password"
-                            value={data.password}
+              id="password"
+              value={data.password}
               onChange={handleChange}
             />
           </div>
